b50ab08efa9f915da646295d28caa68b2ce6ecea
Kevin Robatel <kevin.robatel@qoqa.com>
2019-10-22 14:13:38 +0200

    Remove useless onQueueIdChanged

--- library/src/main/java/com/queue_it/androidsdk/QueueListener.java
+++ library/src/main/java/com/queue_it/androidsdk/QueueListener.java
@@ -1,12 +1,9 @@
 package com.queue_it.androidsdk;
 
-import androidx.annotation.NonNull;
-
 public interface QueueListener {
     void onQueuePassed(QueuePassedInfo queuePassedInfo);
     void onQueueViewWillOpen();
     void onQueueDisabled();
     void onQueueItUnavailable();
-    void onQueueIdChanged(@NonNull String queueId);
     void onError(Error error, String errorMessage);
 }
